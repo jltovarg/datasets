1e861faab6207c97678f646921a47e5e9e80902f
jltovarg74 <jltovarg@gmail.com>
2023-07-08 13:25:29 -0500

    Datasets

---
+++ datos.js
@@ -0,0 +1,11 @@
+[
+    { "partido": "PP", "votantes": 8.5 },
+    { "partido": "PSOE", "votantes": 6.7 },
+    { "partido": "IU", "votantes": 9.2 },
+    { "partido": "UPyD", "votantes": 7.8 },
+    { "partido": "Ciudadanos", "votantes": 5.3 },
+    { "partido": "Podemos", "votantes": 10.0 },
+    { "partido": "Convergència (CDC)", "votantes": 4.9 },
+    { "partido": "ERC", "votantes": 8.1 },
+    { "partido": "ICV", "votantes": 7.4 }
+]
