e907c99fc2a77140199dd5122bffb773f66646d6
jltovarg74 <jltovarg@gmail.com>
2023-07-08 14:09:10 -0500

    add data

---
+++ datosAlumnos.js
@@ -0,0 +1,12 @@
+[
+    {"alumno":"Jose Luis Tovar","nota":10,"ranking":1},
+    {"alumno":"Isabel Rosero","nota":9.5,"ranking":2},
+    {"alumno":"Laura Muñoz","nota":9,"ranking":3},
+    {"alumno":"Natalia Rosero","nota":8.9,"ranking":4},
+    {"alumno":"Tulio Tobar","nota":8.2,"ranking":5},
+    {"alumno":"Ines Guerrero","nota":7.6,"ranking":6},
+    {"alumno":"Rosa Rodriguez","nota":7,"ranking":7},
+    {"alumno":"Carlos Maya","nota":6.9,"ranking":8},
+    {"alumno":"Ricardo Martinez","nota":6.4,"ranking":9}
+
+]
